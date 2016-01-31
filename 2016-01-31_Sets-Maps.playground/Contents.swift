882d6affa674a95f9496cd23e854a14d343c1232
Justine Gartner <GartnerJustine@gmail.com>
2016-01-31 15:07:10 -0500

    Created spellCheck function

--- 2016-01-31_Sets-Maps.playground/Contents.swift
+++ 2016-01-31_Sets-Maps.playground/Contents.swift
@@ -128,7 +128,7 @@ hash("R&B")
 
 struct StringHashSet: SetProtocol, CustomStringConvertible {
     
-    var elements = [String?](count: 128, repeatedValue: nil)
+    private var elements = [String?](count: 128, repeatedValue: nil)
     
     //SetProtocol
     
@@ -153,7 +153,7 @@ struct StringHashSet: SetProtocol, CustomStringConvertible {
     
     var description: String {
         
-        return "{\(genres.elements.filter{$0 != nil}.map{$0!})}"
+        return "{\(elements.filter{$0 != nil}.map{$0!})}"
         //See FILTER and MAP below****************************
     }
     
@@ -174,7 +174,7 @@ genres.remove("Western")
 genres.contains("Western")
 
 genres.add("R&B")
-genres.description
+genres.elements.description
 
 
 print(genres.elements.filter{$0 != nil})
@@ -238,3 +238,220 @@ print(furniture.map(numberOfCharacters))
 //***********REDUCE****************
 
 print(furniture.reduce("foo ", combine: {$0 + $1}))
+
+
+
+
+//**********EXERCISES*************
+
+/*
+Use our set implementation from in class 
+to write a simple spellCheck function that 
+returns true if the string has no spelling errors. 
+
+ - Use a dictionary of 6 words
+ - Test your method both with a string that passes
+    and a string that fails.
+*/
+
+
+
+var dict = StringHashSet()
+dict.add("the")
+dict.add("quick")
+dict.add("brown")
+dict.add("fox")
+dict.add("jumps")
+dict.add("over")
+dict.description
+
+print(dict.elements.filter{$0 != nil})
+
+
+
+func spellCheck(str: String) -> Bool
+{
+
+    let strWords = str.characters.split{$0 == " "}.map(String.init)
+    
+    for word in strWords {
+        
+        //print(word)
+        
+        if dict.contains(word) {
+            
+           continue
+            
+        }else {
+            
+            return false
+        }
+    }
+    
+    return true
+}
+
+
+func spellCheck2(str: String) -> Bool
+{
+    
+    let strWords = str.characters.split{$0 == " "}.map(String.init)
+    
+    for word in strWords {
+        
+        if !dict.contains(word) {
+            
+            return false
+        }
+    }
+    
+    return true
+}
+
+
+
+spellCheck("the quick brown fox jumps over fox brown jumps quick")
+spellCheck("the quick brwon fox jumps over fox brown jumps quick")
+
+
+//******With SWift Set*********
+
+var spellDict = Set<String>()
+
+for word in ["the", "quick", "brown", "fox", "jumps", "over", "truck"] {
+    spellDict.insert(word)
+}
+
+spellDict
+
+func spellCheck3(str: String) -> Bool {
+    for word in str.componentsSeparatedByString(" ") {
+        if !spellDict.contains(word) {
+            return false
+        }
+    }
+    return true
+}
+
+spellCheck3("the quick brown truck jumps over fox brown jumps quick")
+spellCheck3("the quick brwon truck jumps over fox brown jumps quick")
+
+
+//****Good, Bad or Invalid Hash Maps**************
+
+func sumHash(s: String) -> Int {
+    // Sum of all character codes in s
+    return Int(s.unicodeScalars
+        .map{$0.value}
+        .reduce(0, combine: {$0 &+ $1}))
+}
+
+sumHash("Hello")
+sumHash("awesome")
+
+sumHash("rat")
+sumHash("tar")
+
+
+func productHash(s: String) -> Int {
+    return Int(s.unicodeScalars
+        .map{$0.value}
+        .reduce(0, combine: {$0 &* 256 &+ $1}))
+}
+
+productHash("Hello")
+productHash("awesome")
+
+productHash("rat")
+productHash("tar")
+
+
+//invalid
+func dateHash(date: NSDate) -> Int {
+    return Int(date.timeIntervalSinceNow)
+}
+
+
+
+//**************************************************
+//**************************************************
+//**************************************************
+
+abs("asdf".hashValue % 32)
+abs("foo".hashValue % 32)
+abs("racecar".hashValue % 32)
+abs("watermelon".hashValue % 32)
+abs("fruitcake".hashValue % 32)
+abs("fruit".hashValue % 32)
+abs("cake".hashValue % 32)
+abs("sweet".hashValue % 32)
+
+
+//*******************Open Addressing**********************
+
+struct OpenHashSet: SetProtocol, CustomStringConvertible {
+    
+    private var elements = [String?]()
+    
+    init (capacity: Int) {
+        
+        elements = [String?](count: capacity, repeatedValue: nil)
+    }
+    
+    //SetProtocol
+    
+    typealias ItemType = String
+    
+    mutating func add(item: String) {
+        
+        let hashValue = hash(item)
+        
+        if contains(item) {return}
+        
+        for i in 0..<elements.count {
+            
+            if elements[(hashValue + i) % elements.count] == nil {
+                
+                elements[(hashValue + i) % elements.count] = item
+            }
+        }
+        
+    }
+    
+    mutating func remove(item: String) {
+        
+        elements[hash(item)] = nil
+    }
+    
+    func contains(item: String) -> Bool {
+        
+        let hashValue = hash(item)
+        
+        for i in 0..<elements.count {
+            
+            if let element = elements[(hashValue + i) % elements.count] {
+                
+                if element == item {
+                    
+                    return true
+                }
+                
+            }else {
+                
+                return false
+            }
+        }
+        
+        return false
+    }
+    
+    //Custom String Convertible
+    
+    var description: String {
+        
+        return "{\(elements.filter{$0 != nil}.map{$0!})}"
+        
+    }
+    
+}
+
